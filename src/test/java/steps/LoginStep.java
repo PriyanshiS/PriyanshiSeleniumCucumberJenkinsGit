69fe58149225e7f44e8c4add6b18d3a9b7191acc
Priyanshi Saxena <priyanshi1106@gmail.com>
2018-08-27 19:08:11 -0400

    test
    test

--- src/test/java/steps/LoginStep.java
+++ src/test/java/steps/LoginStep.java
@@ -31,7 +31,8 @@ public class LoginStep extends BaseUtil{
     public void iNavigateToTheLoginPage() throws Throwable {
 
         System.out.println("Navigate Login Page");
-        base.Driver.navigate().to("http://www.executeautomation.com/demosite/Login.html");
+        //base.Driver.navigate().to("http://www.executeautomation.com/demosite/Login.html");
+		base.Driver.navigate().to("http://google.ca");
     }
 
 
