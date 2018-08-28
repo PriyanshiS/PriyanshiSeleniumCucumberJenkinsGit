2aa535520df388725aa97a54b6a287965ba9c47c
Priyanshi Saxena <priyanshi1106@gmail.com>
2018-08-27 21:14:44 -0400

    edit logic
    edit logic

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -35,7 +35,7 @@ public class Hook extends BaseUtil{
             System.out.println(scenario.getName());
         }
         System.out.println("Closing the browser : MOCK");
-		base.Driver.quit();
+		
     }
 
 }
